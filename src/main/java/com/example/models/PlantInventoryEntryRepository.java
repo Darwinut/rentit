bdf4bac213eda2645711e80d5714faf1972ac457
dasiva <dasiva@git.ttu.ee>
2016-02-24 19:16:39 +0200

    Added Repositories

--- src/main/java/com/example/models/PlantInventoryEntryRepository.java
+++ src/main/java/com/example/models/PlantInventoryEntryRepository.java
@@ -7,8 +7,12 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface PlantInventoryEntryRepository extends JpaRepository<PlantInventoryEntry, Long>{
-	List<PlantInventoryEntry> findByNameContaining(String str); 
-	  @Query("select p from PlantInventoryEntry p where LOWER(p.name) like ?1 ") 
-	  List<PlantInventoryEntry> finderMethod(String name);
+public interface PlantInventoryEntryRepository extends JpaRepository<PlantInventoryEntry, Long> {
+
+	// Normal query
+	List<PlantInventoryEntry> findByNameContaining(String str);
+
+	// Named / explicit query
+	@Query("select p from PlantInventoryEntry p where LOWER(p.name) like ?1 ")
+	List<PlantInventoryEntry> finderMethod(String name);
 }
---
+++ src/main/java/com/example/models/PlantInventoryItemRepository.java
@@ -0,0 +1,7 @@
+package com.example.models;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PlantInventoryItemRepository extends JpaRepository<PlantInventoryItem, Long> {
+
+}
